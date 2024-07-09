4116fdbe43a25c53c2a94b87e5ec1390e3e45437
Yuri Espinosa Pires <110349970+yuri-pires@users.noreply.github.com>
2024-07-08 22:03:01 -0300

    integração com o servico Via Cep
    classe de busca com o WS Via Cep

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yuri-pires/busca-cep
+
+go 1.22.3
---
+++ request/buscar-cep.go
@@ -0,0 +1,46 @@
+package request
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+type ViaCepResponse struct {
+	CEP         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	UF          string `json:"uf"`
+	IBGE        string `json:"ibge"`
+	GIA         string `json:"gia"`
+	DDD         string `json:"ddd"`
+	SIAFI       string `json:"siafi"`
+}
+
+func BuscarCep(cep string) *ViaCepResponse {
+	var viaCepStruct ViaCepResponse
+	viaCepUrl := "https://viacep.com.br/ws/" + cep + "/json/"
+
+	res, err := http.Get(viaCepUrl)
+	if err != nil {
+		log.Fatalf("Erro ao realizar consulta no WS Via Cep %v", err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Erro ao ler a resposta: %v", err)
+	}
+
+	err = json.Unmarshal(body, &viaCepStruct)
+
+	if err != nil {
+		log.Fatalf("Erro ao transformar resposta: %v", err)
+	}
+
+	return &viaCepStruct
+}
